0664ffc682d613f16d19511a43e1fe36f4267f97
GhumakkadSoul <soulghumakkad@gmail.com>
2024-07-09 18:45:59 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -31,7 +31,7 @@ const Footer = () => {
 
             <p className="flex items-center font-medium justify-normal gap-2">
               <Phone size={17} />
-              +91 9912345678
+              +91 8630024459
             </p>
             <p className="flex mt-4 font-medium items-center justify-normal gap-2">
               <Mail size={17} />
