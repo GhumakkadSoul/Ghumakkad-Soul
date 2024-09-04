771403387d68aee99fed71679a7be15d195c59c5
GameOfCode64 <131631135+GameOfCode64@users.noreply.github.com>
2024-09-04 23:02:35 +0530

    new commit

--- app/(client)/products/page.tsx
+++ app/(client)/products/page.tsx
@@ -7,7 +7,7 @@ const page = async () => {
 
   return (
     <div className="w-full h-full lg:px-20 px-4 my-6">
-      <h1 className="font-bold text-[17px] md:text-3xl  relative heading">
+      <h1 className="font-bold text-[18px] md:text-3xl  relative heading">
         Rent Trekking Gears
       </h1>
       <ProductCard data={data || []} />
